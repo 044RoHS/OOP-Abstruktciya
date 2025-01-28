20ea38f79cf50a171c4272f79520c9a390509810
Artyom <kolmazeff@yandex.ru>
2025-01-28 21:59:21 +0300

    five commit

---
+++ OOP-Address1.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+#include <fstream>
+
+
+
+class Address
+{
+private:
+    std::string Cities;
+    std::string Streets;
+    int NumberHouse;
+    int NumberApartment;
+
+public:
+    Address(std::string Citis, std::string Street, int House, int Apartment)
+    {
+        Cities = Citis;
+        Streets = Street;
+        NumberHouse = House;
+        NumberApartment = Apartment;
+
+
+    }
+    Address() {}
+
+    void getOutputAdderss(std::ofstream& foul) const
+    {
+
+        foul << Cities << " " << Streets << " " << NumberHouse << " " << NumberApartment << std::endl;
+
+
+
+
+
+    }
+
+};
+
+
+
+int main()
+{
+
+    setlocale(LC_ALL, "Ru");
+
+    int CountAddress;
+    std::ifstream fil("in.txt");
+    std::ofstream foul("out1.txt");
+    if (!foul.is_open())
+    {
+        std::cout << " Не получилось открыть файл out.txt" << std::endl;
+    }
+
+    if (!fil.is_open())
+    {
+        std::cout << "Не получилось открыть файл in.txt" << std::endl;
+
+    }
+    fil >> CountAddress;
+    foul << CountAddress << std::endl;
+    Address* array = new Address[CountAddress];
+
+    for (int i = 0; i < CountAddress; ++i)
+    {
+        std::string city;
+        std::string street;
+        int HouseNumber;
+        int ApartmentNumbre;
+        fil >> city >> street >> HouseNumber >> ApartmentNumbre;
+        Address adress(city, street, HouseNumber, ApartmentNumbre);
+        array[i] = Address(city, street, HouseNumber, ApartmentNumbre);
+
+    }
+    std::reverse(array, array + CountAddress);
+    for (int i = 0; i < CountAddress; ++i)
+    {
+        array[i].getOutputAdderss(foul);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    delete[] array;
+    fil.close();
+
+}
